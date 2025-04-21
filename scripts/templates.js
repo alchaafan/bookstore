545b4d32fe16c527861eaec0a6d8c746381a57ca
Muhammad <m.shaafan@outlook.de>
2025-04-21 12:26:26 +0200

    format

--- scripts/templates.js
+++ scripts/templates.js
@@ -2,23 +2,23 @@ function getBooksTemplates(i) {
     return `
              
                                 <div class="book">
-                                               
-                                <h2>${books[i].name}</h2>
+                      
+                                    <h2>${books[i].name}</h2>
                                     <div>${books[i].bild}</div>
-                                    <p> Author: ${books[i].author}</p>                                             
+                                        <p> Author: ${books[i].author}</p>                                             
                                         <div id="likes${i}"><p>${books[i].likes} ${renderFav(i)} </p></div>
-                                                                                                <p>Preis: ${books[i].price} €</p>
-                                        <p>Erscheinungsjahr: ${books[i].publishedYear}</p>  
-                                <div id="commentsContainer${i}"> <input id="commentInput${i}" type="text" placeholder="dein Kommentar">                                                     <button onclick="addComment(${i})" id="sendBtn">&#10148;</button>
-                                                                            <p></p>
-                                                                            
-                                                </div>
+                                                            <p>Preis: ${books[i].price} €</p>
+                                            <p>Erscheinungsjahr: ${books[i].publishedYear}</p>  
+                                         <div id="commentsContainer${i}"> <input id="commentInput${i}"
+                                            type="text" placeholder="dein Kommentar"> 
+                                        <button onclick="addComment(${i})" id="sendBtn">&#10148;</button>                                                <p></p>
+                                    </div>
                                                 
 
-                                             </div>
+                                 </div>
              
              
-                                      `
+              `
 
 }
 
